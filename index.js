1be7390b4e80b88bec8f365472f518523ebb9004
unknown <you@example.com>
2020-03-15 19:09:13 +0800

    ts_02

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-var fn = function () { return 'response'; };
+// for (var i = 0;i<10;i++){
+//     console.log(i)
+// }                                           
+// let a :string  = 'Alex'
+// console.log('my name is'+ a)
+// a = 'Lily'
+// console.log(`my ntame is ${at}`)
+// let tttt  = 1
+// console.log(tttt)
+var b = 1;
--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-var fn = ()=>'response'
+// for (var i = 0;i<10;i++){
+//     console.log(i)
+// }                                           
+
+
+// let a :string  = 'Alex'
+// console.log('my name is'+ a)
+
+// a = 'Lily'
+// console.log(`my ntame is ${at}`)
+// let tttt  = 1
+// console.log(tttt)
+
+var b:any = 1;
